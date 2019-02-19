d791324567cb954d23044429d053b25ddc329bb4
wizubizu <odugbesanabimbola@gmail.com>
2019-02-19 14:47:01 +0100

    new development

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,82 +1,18 @@
- var app = {
- imageHandler:function() {
-        $("#upload").on('change', function() {
-            //alert("alert");
-            readUrl();
-        });
-        function readUrl (){
-            var file = $("#upload")[0].files[0];
-            //console.log(file);
-            //reader interprets the file selected
-            var reader = new FileReader();
-                //console.log(reader.result);
-            reader.onloadend = function (){
-                //console.log(reader.result);
-                $("#picture_preview").attr("src", reader.result);
-                //$("#picture_preview")[0].src = "" + reader.results;
-                //console.log($("#picture_preview"));
-                //console.log(reader.result);
-            }
-            if(file){
-                reader.readAsDataURL(file);
+// gallery
+    function lightbox(idx) {
+            //show the slider's wrapper: this is required when the transitionType has been set to "slide" in the ninja-slider.js
+            var ninjaSldr = document.getElementById("ninja-slider");
+            ninjaSldr.parentNode.style.display = "block";
+
+            nslider.init(idx);
+
+            var fsBtn = document.getElementById("fsBtn");
+            fsBtn.click();
+        }
+
+        function fsIconClick(isFullscreen, ninjaSldr) { //fsIconClick is the default event handler of the fullscreen button
+            if (isFullscreen) {
+                ninjaSldr.parentNode.style.display = "none";
             }
         }
-    }//end of imageHandler
-
-}
-
-var apps = {
- imageHandler:function() {
-        $("#upload1").on('change', function() {
-            //alert("alert");
-            readUrl();
-        });
-        function readUrl (){
-            var file = $("#upload1")[0].files[0];
-            //console.log(file);
-            //reader interprets the file selected
-            var reader = new FileReader();
-                //console.log(reader.result);
-            reader.onloadend = function (){
-                //console.log(reader.result);
-                $("#picture_preview1").attr("src", reader.result);
-                //$("#picture_preview")[0].src = "" + reader.results;
-                //console.log($("#picture_preview"));
-                //console.log(reader.result);
-            }
-            if(file){
-                reader.readAsDataURL(file);
-            }
-        }
-    }//end of imageHandler
-    
-}
-
-var appz = {
- imageHandler:function() {
-        $("#upload2").on('change', function() {
-            //alert("alert");
-            readUrl();
-        });
-        function readUrl (){
-            var file = $("#upload2")[0].files[0];
-            //console.log(file);
-            //reader interprets the file selected
-            var reader = new FileReader();
-                //console.log(reader.result);
-            reader.onloadend = function (){
-                //console.log(reader.result);
-                $("#picture_preview2").attr("src", reader.result);
-                //$("#picture_preview")[0].src = "" + reader.results;
-                //console.log($("#picture_preview"));
-                //console.log(reader.result);
-            }
-            if(file){
-                reader.readAsDataURL(file);
-            }
-        }
-    }//end of imageHandler
-    
-}
-
- 
+    // gallery end
